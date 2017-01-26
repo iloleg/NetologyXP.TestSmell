e3b4f3dbb93571798c3e9c8a0dabad794b9c144e
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-01-26 15:47:10 +0300

    Substring not to contain test

--- tests/string-asserts-tests.js
+++ tests/string-asserts-tests.js
@@ -5,15 +5,20 @@ suite('string asserts', function () {
     });
 
     test('do not check substring for not contains', function () {
-        enterPhoneNumber(true);
+        let phoneValidationError = enterPhoneNumber(true);
 
-        let errorMessage = "Please, enter first name\nPlease enter last name";
+        let errorMessage = "Please, enter first name\nPlease enter last name"
+            + phoneValidationError;
 
         expect(errorMessage).to.not.contain('Please enter phone number');
     });
 
     var enterPhoneNumber = function (isValid) {
+        if (isValid) {
+            return "";
+        }
 
+        return "Please enter phone number";
     };
 
     teardown(function () {
